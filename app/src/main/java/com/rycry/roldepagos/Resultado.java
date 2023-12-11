43f4792baf1e04665293fdfe3ce108fd45feb172
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-12-11 02:35:15 -0500

    metodos para impirmir resultados en activity Resultado

--- app/src/main/java/com/rycry/roldepagos/Resultado.java
+++ app/src/main/java/com/rycry/roldepagos/Resultado.java
@@ -3,6 +3,7 @@ package com.rycry.roldepagos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Resultado extends AppCompatActivity {
 
@@ -10,5 +11,48 @@ public class Resultado extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        // Obtén los datos pasados desde la MainActivity
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String nombre = extras.getString("nombre", "");
+            String cargo = extras.getString("cargo", "");
+            double sueldoFijo = extras.getDouble("sueldoFijo", 0);
+            double subsidioAntiguedad = extras.getDouble("subsidioAntiguedad", 0);
+            double seguroSocial = extras.getDouble("seguroSocial", 0);
+            double pagoHorasExtras = extras.getDouble("pagoHorasExtras", 0);
+            double ingresoPorHijo = extras.getDouble("ingresoPorHijo", 0);
+            double egresosTotales = extras.getDouble("egresosTotales", 0);
+            double ingresosTotales = extras.getDouble("ingresosTotales", 0);
+            double totalRecibir = extras.getDouble("totalRecibir", 0);
+
+            // Muestra los resultados en los TextViews correspondientes
+            TextView tvResultadoNombre = findViewById(R.id.tvResultadoNombre);
+            tvResultadoNombre.setText("Nombre: " + nombre);
+
+            TextView tvResultadoCargo = findViewById(R.id.tvResultadoCargo);
+            tvResultadoCargo.setText("Cargo: " + cargo);
+
+            TextView edResultadoSueldofijo = findViewById(R.id.edResultadoSueldoFijo);
+            edResultadoSueldofijo.setText(String.valueOf(sueldoFijo));
+
+            TextView edResultadoSubsidio = findViewById(R.id.edResultadoSubsidio);
+            edResultadoSubsidio.setText(String.valueOf(subsidioAntiguedad));
+
+            TextView edResultadoSeguro = findViewById(R.id.edResultadoSeguro);
+            edResultadoSeguro.setText(String.valueOf(seguroSocial));
+
+            TextView edResultadoHoras = findViewById(R.id.edResultadoHoras);
+            edResultadoHoras.setText(String.valueOf(pagoHorasExtras));
+
+            TextView edResultadolngresos = findViewById(R.id.edResultadoIngresos);
+            edResultadolngresos.setText(String.valueOf(ingresosTotales));
+
+            TextView edResultadoEgresos = findViewById(R.id.edResultadoEgresos);
+            edResultadoEgresos.setText(String.valueOf(egresosTotales));
+
+            TextView edResultadoTotal = findViewById(R.id.edResultadoTotal);
+            edResultadoTotal.setText(String.valueOf(totalRecibir));
+        }
     }
 }
