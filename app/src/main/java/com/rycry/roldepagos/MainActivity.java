868b33e52d3fb9d6fc829c8d278de77a0f906ca4
RyCry7 <richely.almeida825@ist17dejulio.edu.ec>
2023-12-11 02:20:29 -0500

    creacion de tema y layouts MainActivity y Resultado

--- app/src/main/java/com/rycry/roldepagos/MainActivity.java
+++ app/src/main/java/com/rycry/roldepagos/MainActivity.java
@@ -3,12 +3,48 @@ package com.rycry.roldepagos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtén referencia al Spinner
+        Spinner spinner = findViewById(R.id.spnCargo);
+
+        // Define las opciones en un array de cadenas
+        String[] opciones = {"Junior", "Semi-Senior", "Senior"};
+
+        // Crea un adaptador para el Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+
+        // Especifica el diseño para el menú desplegable
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Aplica el adaptador al Spinner
+        spinner.setAdapter(adapter);
+
+        // Manejar la selección del Spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                // Obtener la opción seleccionada
+                String selectedOption = opciones[position];
+
+                // Puedes hacer algo con la opción seleccionada, por ejemplo, mostrar un mensaje
+                Toast.makeText(MainActivity.this, "Opción seleccionada: " + selectedOption, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Manejar la falta de selección si es necesario
+            }
+        });
     }
 }
